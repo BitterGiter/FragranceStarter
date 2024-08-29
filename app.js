31b329453ffc6e6f4ed18e4165300bf55233e8b0
BitterGiter <j.manning093@gmail.com>
2024-08-28 19:39:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-document.getElementById('fragrance-form').addEventListener('submit', async function(event) {
-    event.preventDefault();
-    const fragrance = document.getElementById('fragrance-input').value;
-    const response = await fetch(`YOUR_API_ENDPOINT?fragrance=${fragrance}`);
-    const data = await response.json();
-    displayResults(data);
-});
-
-function displayResults(data) {
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = '<h2>Complementary Fragrances:</h2>';
-    data.complementaryFragrances.forEach(fragrance => {
-        resultsDiv.innerHTML += `<p>${fragrance}</p>`;
-    });
+function getCatFact() {
+  fetch('https://meowfacts.herokuapp.com/')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('cat-fact').innerText = data.data[0];
+    })
+    .catch(error => console.error('Error:', error));
 }
